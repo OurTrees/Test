ee86ee91a7209892c91fe47279bc6a2404a672b0
applebuddy <applebuddy@nate.com>
2019-07-20 23:15:12 +0900

    TEST

---
+++ TestProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestProject
+//
+//  Created by Min Kyeong Tae on 20/07/2019.
+//  Copyright © 2019 Min Kyeong Tae. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
